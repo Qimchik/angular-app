d4883deaa05d6dbd42de8b709e47b1bddd371058
Dmytro Bohush <you@example.com>
2018-09-07 21:58:33 +0300

    console log removed task3

--- src/app/reducers/reducer.auth.js
+++ src/app/reducers/reducer.auth.js
@@ -1,8 +1,6 @@
 import { Action } from '@ngrx/store'
 
 export function authReducer(state = false, action) {
-  console.log('action', action);
-  console.log('state', state);
   switch (action.type) {
     case 'SIGN_IN':
       return true;
